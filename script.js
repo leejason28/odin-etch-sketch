ffcffaed1db01cb5b5d042e1127b1280b63b0d5a
leejason28 <jason928lee@gmail.com>
2023-05-03 20:32:27 -0700

    need to figure out how to generate new grid to fill the same size as previous one. enforce max size of grid

--- script.js
+++ script.js
@@ -15,28 +15,44 @@ EC: have each pass w/ mouse change to random RGB value. then each pass adds 10% 
 /* main body of html -> going to add all flex containers and other html elements in here */
 const body = document.querySelector('body');
 
+/* title */
 const title = document.createElement('h1');
 title.textContent = "Project: Etch-a-Sketch";
 body.appendChild(title);
 
-/* will leave as 16 for now but should change based on user input */
-const gridSize = 16;
+/* button that asks for user input for size of grid */
+const btn = document.createElement('button');
+btn.textContent = "Click me!"
+btn.addEventListener('click', () => {
+    gridSize = prompt("How many squares per side would you like?");
+    grid.remove();
+    createGrid();
+});
+body.appendChild(btn)
+
+/* size of grid that user can adjust. default 16. max is 100 */
+let gridSize = 16;
+const maxSize = 100;
 
 /* container div (grid) for square divs to fill - filled out one row at a time */
-const grid = document.createElement('div');
-grid.setAttribute('id', 'grid');                                                   /* for css styling */
-for (let i=0; i<gridSize; i++) {
-    const row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    for (let j=0; j<gridSize; j++) {
-        const square = document.createElement('div');
-        square.textContent = "Hello World!"                                        /* for testing: remove later */
-        square.setAttribute('class', 'square');
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'black';
-        })
-        row.appendChild(square);
+function createGrid() {
+    const grid = document.createElement('div');
+    grid.setAttribute('id', 'grid');                                                                         /* for css styling */
+    for (let i=0; i<gridSize; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        for (let j=0; j<gridSize; j++) {
+            const square = document.createElement('div');
+            square.textContent = "Hello World!"                                                              /* for testing: remove later */
+            square.setAttribute('class', 'square');
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'black'; square.style.color = 'black'                         /* currently changes text to black as well (hello world) */
+            });
+            row.appendChild(square);
+        }
+        grid.appendChild(row)
     }
-    grid.appendChild(row)
+    body.appendChild(grid);
 }
-body.appendChild(grid);
+
+createGrid();
